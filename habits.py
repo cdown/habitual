f7043215aae99fe3024245e881a49251647cb49e
Chris Down <chris@chrisdown.name>
2020-09-04 00:18:10 +0100

    Add some todos inline

--- habits.py
+++ habits.py
@@ -5,6 +5,13 @@ import sched
 import os
 import time
 
+# TODO:
+#
+# - Tear down and rebuild scheduler on inotify events
+# - External exec support
+# - Logging at schedule and execution time
+# - Code cleanliness
+
 s = sched.scheduler(time.time)
 
 
