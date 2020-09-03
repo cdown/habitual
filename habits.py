da0a5d30c4ce454fc6f2586acdc61ab8aeed361b
Chris Down <chris@chrisdown.name>
2020-09-04 00:30:31 +0100

    External execution support

--- habits.py
+++ habits.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import crontab
+import subprocess
+import logging
+import argparse
 import sched
 import os
 import time
@@ -8,26 +11,46 @@ import time
 # TODO:
 #
 # - Tear down and rebuild scheduler on inotify events
-# - External exec support
-# - Logging at schedule and execution time
 # - Code cleanliness
 
 s = sched.scheduler(time.time)
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-def notify(title, ct):
-    print(title)
+def notify(name, ct, prog):
+    LOG.info("Running notifier for '%s'", name)
+    ret = subprocess.call([prog, name])
+    if ret != 0:
+        LOG.warning("Command returned %d: %r", ret, [prog, name])
+
     pending_secs = ct.next(default_utc=False)
-    s.enter(pending_secs, 1, notify, argument=(title, ct))
+    LOG.info("Scheduling next run for '%s' in %d seconds", name, pending_secs)
+    s.enter(pending_secs, 1, notify, argument=(name, ct, prog))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-p",
+        "--prog",
+        default="notify-send",
+        help="the program to send notifications to (default: %(default)s)",
+    )
+    return parser.parse_args()
 
 
 def main():
+    args = parse_args()
+
     with open(os.path.expanduser("~/.config/habits/habits")) as f:
         for line in f:
             raw_schedule, name = line.strip().split("|", 1)
             ct = crontab.CronTab(raw_schedule)
             pending_secs = ct.next(default_utc=False)
-            s.enter(pending_secs, 1, notify, argument=(name, ct))
+            LOG.info("Scheduling next run for '%s' in %d seconds", name, pending_secs)
+            s.enter(pending_secs, 1, notify, argument=(name, ct, args.prog))
+
         s.run()
 
 
