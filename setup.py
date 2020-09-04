27294c327b09404faf2dda345105ba535e773426
Chris Down <chris@chrisdown.name>
2020-09-04 01:00:47 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as requirements_f:
 
 setup(
     name="habitual",
-    version="0.1.0",
+    version="0.1.1",
     description="Send notifications for things on a fixed, repeated schedule",
     long_description=README,
     url="https://github.com/cdown/habitual",
